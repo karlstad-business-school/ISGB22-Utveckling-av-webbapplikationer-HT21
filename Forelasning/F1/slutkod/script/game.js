653be331cfede8e2c377200890e88bd631354695
Peter Bellström <peter.bellstrom@kau.se>
2021-11-02 09:35:17 +0100

    Uppdaterat F1 och W1

---
+++ Forelasning/F1/slutkod/script/game.js
@@ -0,0 +1,10 @@
+/*
+    <img src="https://openclipart.org/download/282132/Die6.svg" />
+    <img src="https://openclipart.org/download/282131/Die5.svg" />
+    <img src="https://openclipart.org/download/282130/Die4.svg" />
+    <img src="https://openclipart.org/download/282129/Die3.svg" />
+    <img src="https://openclipart.org/download/282128/Die2.svg" />
+    <img src="https://openclipart.org/download/282127/Die1.svg" />              
+*/
+
+'use strict';
