298d1c8636f9cfae61172d4ce76af04242c98313
peteratkau <peter.bellstrom@kau.se>
2021-11-22 14:18:24 +0100

    Uppdaterat W4 igen...

--- Workshop/W4/slutkod/script/game.js
+++ Workshop/W4/slutkod/script/game.js
@@ -42,6 +42,17 @@ function gameOver() {
 $(document).ready(function() {
     console.log('ready');
 
+    //AJAX-instruktioner på document nivå...
+    $(document).ajaxStart( function() {
+        $('body').fadeTo(2000, 0.3);
+    });
+
+    $(document).ajaxComplete( function() {
+        $('body').fadeTo(2000, 1.0);
+    });
+
+
+
     $(document).on('keydown', function(e) {
         console.log('keydown');
 
@@ -67,95 +78,129 @@ $(document).ready(function() {
                 3. Om .ajax() anropet inte går bra skall du istället till konsolen skriva ut 'fail'.
             */
 
-           $('body h3').remove();
+            let request = $.ajax({
+                'url' : 'ajax/koordinater.php',
+                'method' : 'POST',
+                'dataType' : 'json',
+                'timeout' : '2000',
+                'data' : {
+                    'docHeight' : $(document).height(),
+                    'docWidth' : $(document).width(),
+                    'imgHeight' : oGameData.imgHeight,
+                    'imgWidth' : oGameData.imgWidth
+                }
+            });
 
-            oGameData.timerId = setInterval(function() {
+            request.done( function(inData) {
+                console.log('done');
+                console.log(inData);
 
-                let imgRef = $('<img>');
+                oGameData.imgPositions = inData;
 
-                imgRef.attr('alt', 'Detta är en bild på ett spöke!');
-                imgRef.attr('src', 'https://openclipart.org/image/400px/svg_to_png/83359/fantomme.png');
+                $('body h3').remove();
 
-                imgRef.width('400px');
-                imgRef.height('211px');
-
-                //Nytt
-                /*
-                    B. 
-                    1. Skapa variablerna width, height, left och top.
-
-                    2. Till width-variabeln tilldela värdet som finns i width-egenskapen för aktuellt objekt i oGameData.imgPositions. 
-                    Aktuellt objekt är oGameData.totalGhosts och eftersom du kan behandla imgPostions som en "vektor" kan du använda 
-                    något i stil med globaltObjekt.objekt_med_egenskaper[globaltObjekt.egenskap_för_totalt_antal_spöken].egenskap;
-
-                    3. Till height-variabeln tilldela värdet som finns i height-egenskapen för aktuellt objekt i oGameData.imgPositions. 
-                    Aktuellt objekt är oGameData.totalGhosts och eftersom du kan behandla imgPostions som en "vektor" kan du använda 
-                    något i stil med globaltObjekt.objekt_med_egenskaper[globaltObjekt.egenskap_för_totalt_antal_spöken].egenskap;
-
-                    4. Till left-variabeln tilldela värdet som finns i left-egenskapen för aktuellt objekt i oGameData.imgPositions. 
-                    Aktuellt objekt är oGameData.totalGhosts och eftersom du kan behandla imgPostions som en "vektor" kan du använda 
-                    något i stil med globaltObjekt.objekt_med_egenskaper[globaltObjekt.egenskap_för_totalt_antal_spöken].egenskap;
-
-                    5. Till top-variabeln tilldela värdet som finns i top-egenskapen för aktuellt objekt i oGameData.imgPositions. 
-                    Aktuellt objekt är oGameData.totalGhosts och eftersom du kan behandla imgPostions som en "vektor" kan du använda 
-                    något i stil med globaltObjekt.objekt_med_egenskaper[globaltObjekt.egenskap_för_totalt_antal_spöken].egenskap;
-                */
-
-                let width = imgRef.width() - Math.round( imgRef.width() / 25) * oGameData.antalSpoken;
-                let height = imgRef.height() - Math.round( imgRef.height() / 25) * oGameData.antalSpoken;
-                
-                let top = Math.round( Math.random() * ($(document).height() - height ) ) + 1;
-                let left = Math.round( Math.random() * ($(document).width() - width ) ) + 1;
-
+                oGameData.timerId = setInterval(function() {
     
-                imgRef.css('position',  'absolute');
-                imgRef.css('left', left + 'px');
-                imgRef.css('top', top + 'px');
-                
-                imgRef.css({'height' : height + 'px'})
-                imgRef.css({'width' : width + 'px'})
-                
-
-                console.log(height, width);
-
-                imgRef.css({'z-index' : oGameData.antalSpoken, 'cursor' : 'crosshair'});    
-
-                imgRef.on('mouseover', function(e) {
-                    oGameData.antalKlickadSpoken++;
-                    clearTimeout($(this).attr('data-timerid'));
-
-                    $(this).off('mouseover');
-                    $(this).fadeOut('slow', function() {
-                        $(this).remove();
-                    });    
+                    let imgRef = $('<img>');
+    
+                    imgRef.attr('alt', 'Detta är en bild på ett spöke!');
+                    imgRef.attr('src', 'https://openclipart.org/image/400px/svg_to_png/83359/fantomme.png');
+    
+                    //imgRef.width('400px');
+                    //imgRef.height('211px');
+    
+                    //Nytt
+                    /*
+                        B. 
+                        1. Skapa variablerna width, height, left och top.
+    
+                        2. Till width-variabeln tilldela värdet som finns i width-egenskapen för aktuellt objekt i oGameData.imgPositions. 
+                        Aktuellt objekt är oGameData.totalGhosts och eftersom du kan behandla imgPostions som en "vektor" kan du använda 
+                        något i stil med globaltObjekt.objekt_med_egenskaper[globaltObjekt.egenskap_för_totalt_antal_spöken].egenskap;
+    
+                        3. Till height-variabeln tilldela värdet som finns i height-egenskapen för aktuellt objekt i oGameData.imgPositions. 
+                        Aktuellt objekt är oGameData.totalGhosts och eftersom du kan behandla imgPostions som en "vektor" kan du använda 
+                        något i stil med globaltObjekt.objekt_med_egenskaper[globaltObjekt.egenskap_för_totalt_antal_spöken].egenskap;
+    
+                        4. Till left-variabeln tilldela värdet som finns i left-egenskapen för aktuellt objekt i oGameData.imgPositions. 
+                        Aktuellt objekt är oGameData.totalGhosts och eftersom du kan behandla imgPostions som en "vektor" kan du använda 
+                        något i stil med globaltObjekt.objekt_med_egenskaper[globaltObjekt.egenskap_för_totalt_antal_spöken].egenskap;
+    
+                        5. Till top-variabeln tilldela värdet som finns i top-egenskapen för aktuellt objekt i oGameData.imgPositions. 
+                        Aktuellt objekt är oGameData.totalGhosts och eftersom du kan behandla imgPostions som en "vektor" kan du använda 
+                        något i stil med globaltObjekt.objekt_med_egenskaper[globaltObjekt.egenskap_för_totalt_antal_spöken].egenskap;
+                    */
+    
+                    /*
+                    let width = imgRef.width() - Math.round( imgRef.width() / 25) * oGameData.antalSpoken;
+                    let height = imgRef.height() - Math.round( imgRef.height() / 25) * oGameData.antalSpoken;
                     
-                });
+                    let top = Math.round( Math.random() * ($(document).height() - height ) ) + 1;
+                    let left = Math.round( Math.random() * ($(document).width() - width ) ) + 1;
+                    */
+                    oGameData.antalSpoken++;
 
-                let timerId = setTimeout(function() {
-                    console.log('imgRef.remove()', Date.now())
-                    oGameData.missadeSpoken++;
-                    clearTimeout(imgRef.attr('data-timerid'));
+                    let width = oGameData.imgPositions[oGameData.antalSpoken].width;
+                    let height = oGameData.imgPositions[oGameData.antalSpoken].height;
+                    let left = oGameData.imgPositions[oGameData.antalSpoken].left;
+                    let top = oGameData.imgPositions[oGameData.antalSpoken].top;
+        
+                    imgRef.css('position',  'absolute');
+                    imgRef.css('left', left + 'px');
+                    imgRef.css('top', top + 'px');
+                    
+                    imgRef.css({'height' : height + 'px'})
+                    imgRef.css({'width' : width + 'px'})
+                    
+    
+                    console.log(height, width);
+    
+                    imgRef.css({'z-index' : oGameData.antalSpoken, 'cursor' : 'crosshair'});    
+    
+                    imgRef.on('mouseover', function(e) {
+                        oGameData.antalKlickadSpoken++;
+                        clearTimeout($(this).attr('data-timerid'));
+    
+                        $(this).off('mouseover');
+                        $(this).fadeOut('slow', function() {
+                            $(this).remove();
+                        });    
+                        
+                    });
+    
+                    let timerId = setTimeout(function() {
+                        console.log('imgRef.remove()', Date.now())
+                        oGameData.missadeSpoken++;
+                        clearTimeout(imgRef.attr('data-timerid'));
+    
+    
+                        imgRef.animate({'left' : $(document).width(), 'top' : $(document).height()},
+                        1000, 'linear', function() {
+                             imgRef.remove();
+                        });    
+                       
+    
+                        if(oGameData.missadeSpoken === 3) {
+                            console.log('Game Over!');
+                            gameOver();
+                        }
+    
+                    }, 3000 - (oGameData.antalSpoken * 100));
+    
+                    imgRef.attr('data-timerid', timerId);
+                    imgRef.appendTo('body');
+                    
+                    
+    
+                }, 1000 - (oGameData.antalSpoken * 10));
 
+            });
 
-                    imgRef.animate({'left' : $(document).width(), 'top' : $(document).height()},
-                    1000, 'linear', function() {
-                         imgRef.remove();
-                    });    
-                   
-
-                    if(oGameData.missadeSpoken === 3) {
-                        console.log('Game Over!');
-                        gameOver();
-                    }
-
-                }, 3000 - (oGameData.antalSpoken * 100));
-
-                imgRef.attr('data-timerid', timerId);
-                imgRef.appendTo('body');
-                
-                oGameData.antalSpoken++;
-
-            }, 1000 - (oGameData.antalSpoken * 10));
+            request.fail( function(jqXHR) {
+                console.log('fail');
+                console.log(jqXHR);
+            });
+       
         }
     });
 });
