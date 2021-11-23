0b52a4f72ce2f457d7a064802369c7586e6b3296
peteratkau <peter.bellstrom@kau.se>
2021-11-23 11:03:53 +0100

    Uppdaterat F5

--- Forelasning/F5/rep/ajax/rep.php
+++ Forelasning/F5/rep/ajax/rep.php
@@ -1,3 +1,6 @@
 <?php
+    //sleep() gör att servern väntar ett antal sekunder innan den fortsätter med koden som följer.
     //sleep(4);
+
+    //Texten som servern returnerar...
     echo("AJAX returnerar: Här kommer lite data från servern!");
--- Forelasning/F5/rep/script/rep.js
+++ Forelasning/F5/rep/script/rep.js
@@ -1,2 +1,37 @@
 'use strict';
 
+$(document).ready(function() {
+
+    console.log('ready');
+
+    $(document).on('keydown', function(e) {
+
+        if(e.key === 'a' || e.key === 'A') {
+
+            let request = $.ajax({
+                'method' : 'GET',
+                'url' : 'ajax/rep.php',
+                'dataType' : 'text',
+                'timeout' : '2000'
+            });
+
+            request.fail( function(jqXHR) {
+                console.log(jqXHR);
+                alert(jqXHR.statusText);
+            });
+
+            request.done(function(inData) {
+                console.log(inData);
+
+                //$('<p>').text(inData + ' ' + Date.now()).appendTo('main');
+                //Kedja av metodanrop medan nedanstående exempel delar upp det i flera med en referens...
+                let pRef = $('<p>');
+                pRef.text(inData + ' ' + Date.now());
+                pRef.appendTo('main');
+            })
+
+        }
+
+    });
+
+});
